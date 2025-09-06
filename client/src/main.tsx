871394b9d33fe5a4fc2213bf3c57c36ca6f3bb37
Noor Alyafi <nooralyafi14@gmail.com>
2025-09-06 12:37:01 +0300

    add Vercel Analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,8 @@ import { HashRouter } from "react-router-dom"
 import App from "./App"
 import "./index.css"
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/next"
+
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
